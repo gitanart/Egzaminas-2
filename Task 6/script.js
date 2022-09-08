3723566eba27141e0fd9ecc909cba474556be34a
admin <admin@admins-MacBook-Air.local>
2022-09-08 17:54:31 +0300

    sixth task done

--- Task 6/script.js
+++ Task 6/script.js
@@ -10,7 +10,7 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 
 const users = [
   { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
+  { id: '2', name: 'John Smith', age: 24 },
   { id: '3', name: 'Tom Jones', age: 31 },
   { id: '4', name: 'Rose Peterson', age: 17 },
   { id: '5', name: 'Alex John', age: 25 },
@@ -18,4 +18,18 @@ const users = [
   { id: '7', name: 'Elton Smith', age: 16 },
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
-];
+]
+
+function getUserAverageAge(arr) {
+    return (users.reduce((acc, {age}) => (acc + age), 0)) / arr.length;
+}
+  
+console.log(getUserAverageAge(users));
+
+
+
+const getUsersName = users.map(function(element) {
+  return `${element.name}`;
+})
+     
+console.log(getUsersName);
